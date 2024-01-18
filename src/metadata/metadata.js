3927d1121163df04e6ecd5732108ad3a50878e86
MariamAlz <100044533@ku.ac.ae>
2024-01-19 03:32:11 +0400

    Add existing project files to Git

--- src/metadata/metadata.js
+++ src/metadata/metadata.js
@@ -9,14 +9,6 @@ module.exports = {
     'GitHub': `<a href="https://github.com/MariamAlz"><i class="fa-brands fa-github fact-icon"></i>MariamAlz</a>`,
     'Email': '<a href="mailto:mariam.mk.alzaabi@gmail.com"><i class="fa fa-envelope fact-icon"></i>mariam.mk.alzaabi@gmail.com</a>'
   },
-  skills: [
-    ['Artificial Intelligence', 100],
-    ['Data Science', 100],
-    ['Python', 100],
-    ['NLP', 100],
-    ['Computer Vision', 100],
-    ['Embedded Systems', 60],
-  ],
   positions: [
     {
       title: 'Graduate Research / Teaching Assistant',
@@ -34,14 +26,6 @@ Prince Hamlet is visited by his father's ghost and ordered to avenge his father'
 Viola finds herself shipwrecked in Illyria and, assuming that her brother Sebastian has died in the wreck, disguises herself as a man to gain a position in Duke Orsino's court. Orsino sends Viola (whom he knows as Cesario) to deliver a message to his love, Olivia. Olivia, however, dislikes the Duke. She falls in love with Viola, who she thinks is a man. Eventually, Viola's brother Sebastian, who in fact was unharmed in the wreck, reappears. At a critical moment, Viola's true identity is revealed when members of the court notice the similarities between her and Sebastian. Olivia quickly falls in love with Sebastian, and Viola confesses her love for the Duke.
 `
     },
-    {
-      title: 'The Tragedy of Romeo and Juliet',
-      period: '1595–1596, with a possible early draft written in 1591',
-      skills: ['tragedy', 'death', 'love'],
-      contents: `
-In Verona, Italy, two families, the Montagues and the Capulets, are in the midst of a bloody feud. Romeo, a Montague, and Juliet, a Capulet, fall in love and struggle to maintain their relationship in the face of familial hatred. After Romeo kills Juliet's cousin Tybalt in a fit of passion, things fall apart. Both lovers eventually commit suicide within minutes of each other, and the feuding families make peace over their recent grief.
-`
-    }
   ],
   experience: [
     {
